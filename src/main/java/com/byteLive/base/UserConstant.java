64c4833850eb9d52c4520cac1d99df15cb45c79f
byteLive <pocketchen@outlook.com>
2025-04-14 23:03:07 +0800

    全局变量&组件执行顺序

---
+++ src/main/java/com/byteLive/base/UserConstant.java
@@ -0,0 +1,16 @@
+package com.byteLive.base;
+
+public class UserConstant {
+    /**
+     * 用户 ID Key
+     */
+    public static final String USER_ID_KEY = "userId";
+    /**
+     * 用户名 Key
+     */
+    public static final String USER_NAME_KEY = "userName";
+    /**
+     * 用户真实名称 Key
+     */
+    public static final String REAL_NAME_KEY = "realName";
+}
